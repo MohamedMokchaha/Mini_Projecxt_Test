e0243708c7a11fffc329dc63db0bfae16459d10b
MohamedMokchaha <mohamedmokchaha8@gmail.com>
2023-04-30 00:56:56 +0100

    Add Some Update

--- app/src/main/java/com/example/myloginapp/SecondActivity.java
+++ app/src/main/java/com/example/myloginapp/SecondActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.gms.common.data.BitmapTeleporter;
@@ -15,15 +17,19 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class SecondActivity extends AppCompatActivity {
     FirebaseAuth auth;
-    Button button;
+    ImageButton button;
+    Button add_product;
     TextView textView;
     FirebaseUser user;
-
+    EditText nom, description_p,price_p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        add_product = findViewById(R.id.add);
+        nom = findViewById(R.id.nom_p);
+        description_p = findViewById(R.id.description_p);
+        price_p = findViewById(R.id.price);
         auth = FirebaseAuth.getInstance();
         button = findViewById(R.id.logout);
         textView = findViewById(R.id.user_details);
